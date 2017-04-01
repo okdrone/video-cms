2ed8fec0c59e38b6f0b87135a11780565a1d7658
gewanbo@gmail.com <gewanbo@gmail.com>
2017-04-02 00:12:15 +0800

    sync from old branch

---
+++ data/conf/db.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * 配置文件
+ */
+return array(
+    'DB_TYPE' => 'mysql',
+    'DB_HOST' => 'localhost',
+    'DB_NAME' => 'yuntehu_db',
+    'DB_USER' => 'root',
+    'DB_PWD' => '1234567890',
+    'DB_PORT' => '3306',
+    'DB_PREFIX' => 'yth_',
+    //密钥
+    "AUTHCODE" => '4XqT10FWgfX3cBwJ4F',
+    //cookies
+    "COOKIE_PREFIX" => 'r5ky67_',
+);
