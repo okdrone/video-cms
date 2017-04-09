eb6f8966bf9975cff41f29bfc1b4b75f57ffbab8
gewanbo <gewanbo@gmail.com>
2017-04-09 23:36:02 +0800

    Update

--- src/js/question.js
+++ src/js/question.js
@@ -16,6 +16,13 @@ function count_selected_questions() {
 
     $('.question-status .text .x').html(question_selected_count);
     $('.question-status .text .y').html(question_count);
+
+    var button = $('.question-status .btn button');
+    if(question_selected_count === question_count){
+        button.removeAttr('disabled');
+    } else {
+        button.attr('disabled', 'disabled');
+    }
 }
 
 count_selected_questions();
@@ -24,4 +31,17 @@ $(questionItem).find('li').click(function () {
     $(this).parent().find('li').removeClass('selected');
     $(this).addClass('selected');
     count_selected_questions();
+});
+
+$('#question_submit').click(function () {
+    console.log('ok');
+
+    $(questionItem).each(function (i, e) {
+        $(e).find('li').each(function (ii, ee) {
+            if($(ee).hasClass('selected')){
+                var data = $(ee).find('.option').attr('data');
+                console.log(data);
+            }
+        });
+    });
 });
