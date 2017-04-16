f95d5f6cc7becd56692f653ba5d93b6dd64103f0
gewanbo <gewanbo@gmail.com>
2017-04-16 13:37:26 +0800

    Update

--- application/Common/Controller/WechatController.class.php
+++ application/Common/Controller/WechatController.class.php
@@ -14,8 +14,6 @@ class WechatController extends Controller {
     function getCode(){
         $redirect = 'http://' . $_SERVER['HTTP_HOST'] . U('Video/Index/receiveCode');
 
-        $redirect = 'http://dev.okdrone.org' . U('Video/Index/receiveCode');
-
         $url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid='.C('WE_APPID').'&redirect_uri=' . urlencode($redirect) . '&response_type=code&scope=snsapi_base&state=code#wechat_redirect';
 
         header('Location: '. $url);
