a620795c03eaf56bd86895ec8c0664cc6351c3fd
gewanbo <gewanbo@gmail.com>
2017-04-17 22:30:28 +0800

    update

--- application/Common/Controller/WechatController.class.php
+++ application/Common/Controller/WechatController.class.php
@@ -14,7 +14,7 @@ class WechatController extends Controller {
     function getWebCode(){
         $redirect = 'http://' . $_SERVER['HTTP_HOST'] . U('Video/Index/receiveCode');
 
-        $url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid='.C('WE_APPID').'&redirect_uri=' . urlencode($redirect) . '&response_type=code&scope=snsapi_userinfo&state=code#wechat_redirect';
+        $url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid='.C('WE_APPID').'&redirect_uri=' . urlencode($redirect) . '&response_type=code&scope=snsapi_base&state=code#wechat_redirect';
 
         header('Location: '. $url);
     }
