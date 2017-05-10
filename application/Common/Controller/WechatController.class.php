a01beca4fa3f15de2c09bd624dcb9fc89cd272c9
gewanbo <gewanbo@gmail.com>
2017-05-10 23:49:32 +0800

    Setting timeout value for http api.

--- application/Common/Controller/WechatController.class.php
+++ application/Common/Controller/WechatController.class.php
@@ -85,6 +85,8 @@ class WechatController extends Controller {
         curl_setopt($ch, CURLOPT_HEADER, 0);
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
         $result = curl_exec($ch);
         curl_close($ch);
         return $result;
